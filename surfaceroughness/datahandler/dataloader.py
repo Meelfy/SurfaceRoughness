9f8121ac221d11e0390000f8467ada9a67081591
Meelfy <535370561@qq.com>
2019-05-17 02:18:07 +0800

    data sampling

--- surfaceroughness/datahandler/dataloader.py
+++ surfaceroughness/datahandler/dataloader.py
@@ -1,4 +1,9 @@
+import logging
+import random
+
 import numpy as np
+
+logger = logging.getLogger(__name__)
 
 
 class DataLoader(object):
@@ -7,10 +12,20 @@ class DataLoader(object):
     """
 
     def __init__(self):
-        self.data = None
+        self.max_point_number = -1
+        self.sampling_ratio = 1.0
+        self.should_shuffle = False
 
-    @staticmethod
-    def load_from_file(datafile_path: str, filetype: str = 'asc'):
+    def load_data(self, data_path, max_point_number: int = None, sampling_ratio: float = None):
+        if sampling_ratio is not None:
+            # If the sampling ratio is set, the data will be shuffled
+            self.should_shuffle = True
+            self.sampling_ratio = sampling_ratio
+        if max_point_number is not None:
+            self.max_point_number = max_point_number
+        return self.load_from_file(data_path)
+
+    def load_from_file(self, datafile_path: str, filetype: str = 'asc'):
         """
         This function will skip first and end line of the asc file
         :param datafile_path:
@@ -18,19 +33,29 @@ class DataLoader(object):
         :return:
         """
         assert datafile_path.endswith(filetype), f"except a filetype of {filetype} but get {datafile_path}"
+        logger.info(f"load 3d points data from {datafile_path}")
         if filetype == "asc":
             points_3d = []
             with open(datafile_path, encoding="utf-8") as f_in:
-                file_data = f_in.readlines()
+                # remove file and end line which is useless
+                file_data = f_in.readlines()[1:-1]
+                # get samples number limit
+                if self.max_point_number == -1:
+                    self.max_point_number = len(file_data)
+                self.max_point_number = min(self.max_point_number, int(self.sampling_ratio * len(file_data)))
+                print(self.sampling_ratio * len(file_data))
+
+                if self.should_shuffle:
+                    random.shuffle(file_data)
+
                 for idx, line in enumerate(file_data):
-                    if idx == 0 or idx == len(file_data) - 1:
-                        continue
                     points_3d.append([float(e) for e in line.split()])
+                    if len(points_3d) >= self.max_point_number:
+                        # reach the max sample limitation
+                        break
             points_3d = np.array(points_3d)
         else:
             points_3d = None
+            logging.error("filetype is not supported")
             assert f"unsupported filetype {filetype}"
         return points_3d
-
-    def load_data(self, data_path):
-        return self.load_from_file(data_path)
--- surfaceroughness/tests/datahandler/dataloader_test.py
+++ surfaceroughness/tests/datahandler/dataloader_test.py
@@ -6,9 +6,24 @@ from surfaceroughness.datahandler import DataLoader
 
 class TestDataloader(SurfaceRoughnessTestCase):
     def test_load_from_file(self):
-        dataloader = DataLoader()
         asc_file_path = self.FIXTURES_ROOT / "asc_files" / "asc_file_test_case_1.asc"
         asc_file_path = str(asc_file_path)
-        data = dataloader.load_from_file(asc_file_path)
+
+        data_loader = DataLoader()
+        data = data_loader.load_from_file(asc_file_path)
         assert data.shape == (12, 3)
+        assert data.dtype == np.float
+
+    def test_load_data(self):
+        asc_file_path = self.FIXTURES_ROOT / "asc_files" / "asc_file_test_case_1.asc"
+        asc_file_path = str(asc_file_path)
+
+        data_loader = DataLoader()
+        data = data_loader.load_data(asc_file_path, sampling_ratio=0.5)
+        assert data.shape == (6, 3)
+        assert data.dtype == np.float
+
+        # IMPORTANT: the data_loader.sampling_ratio = 0.5, so the max_point_number = min(5, 6) = 5
+        data = data_loader.load_data(asc_file_path, max_point_number=5)
+        assert data.shape == (5, 3)
         assert data.dtype == np.float
